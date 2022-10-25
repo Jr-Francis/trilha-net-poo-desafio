0a7976b810c465c320edd70bfa6937e031a10aff
Francisco Jr <fransales94jr@gmail.com>
2022-10-25 16:21:56 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
+var aparelho1 = new Nokia(numero: "11 111111111", modelo: "Nokia 12", memoria: "128");
+aparelho1.InstalarAplicativo("Instagram");
+aparelho1.InstalarAplicativo("Whatsapp");
+aparelho1.Ligar();
+Console.WriteLine("");
+Console.WriteLine("-----------------------------------------------");
+Console.WriteLine("");
+
+var aparelho2 = new Iphone(numero: "22 222222222", modelo: "Iphone 12 Pro MAX", memoria: "256");
+aparelho2.InstalarAplicativo("Instagram");
+aparelho2.InstalarAplicativo("Whatsapp");
+aparelho2.ReceberLigacao();
+
+
+
+
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
